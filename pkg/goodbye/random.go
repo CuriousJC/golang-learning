554678e6b16d1a255ef2fd5fa7a2aa1b46489e2a
Justin Crosby <justin.crosby@gmail.com>
2024-04-16 16:14:35 -0400

    adding another random just to test

---
+++ pkg/goodbye/random.go
@@ -0,0 +1,16 @@
+package goodbye
+
+import "math/rand"
+import "time"
+
+func Get() int {
+	// User must pass in number of integers to generate
+
+	err := error(nil)
+	if err != nil { // Maybe they didn't pass an integer
+		panic(err)
+	}
+	start := time.Now()
+	rand.Seed(start.UnixNano())
+	return rand.Intn(256)
+}
